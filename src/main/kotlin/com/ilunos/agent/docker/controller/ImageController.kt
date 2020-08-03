f772b8d1888885b32c06f9f045da2049a0608957
mhaenen <Marlon.Haenen@it-novum.com>
2020-08-03 21:30:06 +0200

    Add ImageController

---
+++ src/main/kotlin/com/ilunos/agent/docker/controller/ImageController.kt
@@ -0,0 +1,36 @@
+package com.ilunos.agent.docker.controller
+
+import com.github.dockerjava.api.command.InspectImageResponse
+import com.github.dockerjava.api.model.Image
+import com.ilunos.agent.docker.service.DockerContext
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import java.util.*
+import javax.inject.Inject
+
+@Controller("/images")
+class ImageController {
+
+    @Inject
+    private lateinit var docker: DockerContext
+
+    @Get("{?dangling,name,labels}")
+    fun list(dangling: Optional<Boolean>, name: Optional<String>, labels: Optional<List<String>>): HttpResponse<List<Image>> {
+        return HttpResponse.ok(docker.listImages(dangling, name, labels))
+    }
+
+    @Get("/{id}")
+    fun inspect(id: String): HttpResponse<InspectImageResponse> {
+        return HttpResponse.ok(docker.inspectImage(id))
+    }
+
+    @Delete("{id}{?force,no-prune}")
+    fun delete(id: String, @QueryValue("force") force: Optional<Boolean>, @QueryValue("no-prune") noPrune: Optional<Boolean>): HttpResponse<Nothing> {
+        docker.deleteImage(id, force, noPrune)
+
+        return HttpResponse.ok()
+    }
+}
