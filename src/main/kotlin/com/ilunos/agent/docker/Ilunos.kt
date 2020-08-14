f99a60aac8637e2b3d99146c0952de933397556e
mhaenen <Marlon.Haenen@it-novum.com>
2020-08-15 01:09:41 +0200

    Add some more Logging to see whats going on at startup

--- src/main/kotlin/com/ilunos/agent/docker/Ilunos.kt
+++ src/main/kotlin/com/ilunos/agent/docker/Ilunos.kt
@@ -1,10 +1,16 @@
 package com.ilunos.agent.docker
 
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Context
+import io.micronaut.context.env.Environment
+import io.micronaut.context.exceptions.BeanInstantiationException
 import io.micronaut.runtime.Micronaut.build
+import io.micronaut.security.oauth2.client.DefaultOpenIdProviderMetadata
 import org.slf4j.LoggerFactory
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 
 fun main(args: Array<String>) {
@@ -14,17 +20,61 @@ fun main(args: Array<String>) {
             .start()
 }
 
-class Ilunos {
+@Context
+class Ilunos(context: ApplicationContext, environment: Environment) {
+
+    init {
+        val security = environment.getProperty("micronaut.security.enabled", Boolean::class.java, true)!!
+        val basicAuth = environment.getProperty("micronaut.security.basic-auth.enabled", Boolean::class.java, true)!!
+        val oauthAuth = environment.getProperty("micronaut.security.oauth2.enabled", Boolean::class.java, false)!!
+
+        if (security) {
+            if (basicAuth) {
+                logger.info("Basic Auth is enabled, registering default accounts admin:admin, manager:manager & user:user")
+            }
+
+            if (oauthAuth) {
+                logger.info("OAuth2 Auth is enabled, verifying Connectivity...")
+                try {
+                    context.getBean(DefaultOpenIdProviderMetadata::class.java)
+                } catch (e: BeanInstantiationException) {
+                    logger.error("Unable to communicate with OpenId Provider: ${e.cause?.cause?.message}")
+                    exitProcess(206)
+                }
+            }
+
+            if (!basicAuth && !oauthAuth) {
+                logger.error("Neither Basic Auth or OAuth2 is enabled, At least one required! Please enable one AuthProvider or disable Security.")
+                exitProcess(205)
+            }
+        } else {
+            if (basicAuth || oauthAuth) {
+                logger.warn("Detected enabled AuthProvider but security is disabled. You need to enable security for Authentication to work.")
+            }
+
+            logger.warn("!!! ⚠⚠ Running Agent without Security ⚠⚠ !!! This is not recommended as everyone with access to the agent will be able to control docker!")
+        }
+    }
+
     companion object {
         const val VERSION: String = "1.0.0"
+        private val logger = LoggerFactory.getLogger(Ilunos::class.java)
         val runningInDocker: Boolean = isRunningInsideDocker()
 
         private fun isRunningInsideDocker(): Boolean {
             try {
-                Files.lines(Paths.get("/proc/1/cgroup")).use { stream -> return stream.anyMatch { line: String -> line.contains("/docker").also {
-                    LoggerFactory.getLogger(Ilunos::class.java).info("Detected Docker Environment. Applying Docker specific settings.")
-                } } }
+                Files.lines(Paths.get("/proc/1/cgroup")).use { stream ->
+                    return stream.anyMatch { line: String ->
+                        line.contains("/docker").also {
+                            if (it)
+                                logger.info("Detected Docker Environment. Applying Docker specific settings.")
+                            else
+                                logger.info("Running Docker-Agent in default mode")
+                        }
+                    }
+                }
             } catch (e: IOException) {
+                logger.info("Running Docker-Agent in default mode")
                 return false
             }
         }
